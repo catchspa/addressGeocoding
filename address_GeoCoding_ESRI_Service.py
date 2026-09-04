20e8fe59015937efa52a2328dc75a771850e2f4c
Madan C <madan.challa@afp.gov.au>
2026-09-04 11:12:49 +1000

    add function to handle different start indexes

--- address_GeoCoding_ESRI_Service.py
+++ address_GeoCoding_ESRI_Service.py
@@ -81,45 +81,53 @@ def geocode_arcgis(address):
     
     return lat, lon, "OK"
 
-# Read addresses + unique IDs from CSV
-input_rows = []
-with open("addresses_34000.csv", newline="", encoding="utf-8") as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        input_rows.append((row["unique_id"], row["address"]))
 
-total = len(input_rows)
+def process_input_addresses(input_file="addresses.csv", start_line_number=1, count=20000, start_time=None, filename_suffix=None):
+    # Read addresses + unique IDs from CSV
+    # input_rows = []
+    start_index = start_line_number - 1
+    end_index = start_index + count - 1
 
+    end_line_number = end_index + 1
 
-total = len(input_rows)
-print(f"Loaded {total} rows from GitHub.")
+    output_file = f"geocoded_output_{filename_suffix}.csv" if filename_suffix else f"geocoded_output_{end_line_number}.csv"
 
+    with open(input_file, newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
 
-# -----------------------------
-# PROCESS + WRITE OUTPUT CSV
-# -----------------------------
-start_time = time.time()  # total timer start
+        with open(output_file, "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(
+                f,
+                fieldnames=["unique_id",
+                            "address",
+                            "lat_wgs84", "lon_wgs84",
+                            "lat_gda94", "lon_gda94"]
+            )
 
-# Write results to output CSV
-with open("geocoded_output_34000.csv", "w", newline="", encoding="utf-8") as f:
-    writer = csv.DictWriter(
-        f,
-        fieldnames=["unique_id",
-                    "address",
-                    "lat_wgs84", "lon_wgs84",
-                    "lat_gda94", "lon_gda94"]
-    )
-    writer.writeheader()
+            for index, row in enumerate(reader, start=start_index):
+                #input_rows.append((row["unique_id"], row["address"]))
+                process_row(row, writer, index+1, start_time=start_time, total=count)
+                if index >= end_index:
+                    break
 
-    for index, (unique_id, addr) in enumerate(input_rows, start=1):
+    return output_file, end_line_number
+
+    # total = len(input_rows)
+    #
+    # print(f"Loaded {total} rows from GitHub.")
+
+    # -----------------------------
+    # PROCESS + WRITE OUTPUT CSV
+    # -----------------------------
+def process_row(row, writer, current_line_number, start_time, total=2000):
+
+        (unique_id, addr) = (row["unique_id"], row["address"])
+        lat, lon, status = (None, None, "None")
+        lat, lon, status = geocode_arcgis(addr)
         elapsed = time.time() - start_time
 
-        #lat, lon, status = geocode_address(addr)
-        lat, lon, status = geocode_arcgis(addr)
-
-
         if lat is None:
-            print(f"    Error → {status}")
+            print(f"    [line_no:{current_line_number}] Error → {status}")
             writer.writerow({
                 "unique_id": unique_id,
                 "address": addr,
@@ -128,7 +136,7 @@ with open("geocoded_output_34000.csv", "w", newline="", encoding="utf-8") as f:
             })
         else:
             lat_gda94, lon_gda94 = wgs84_to_gda94(lat, lon)
-            print(f"[{index}/{total}] Processing {unique_id}: {addr} … Success (elapsed {elapsed:.1f}s),  {lat},  {lon},  {lat_gda94},  {lon_gda94}")
+            print(f"[{current_line_number}/{total}] Processing {unique_id}: {addr} … Success (elapsed {elapsed:.1f}s),  {lat},  {lon},  {lat_gda94},  {lon_gda94}")
             writer.writerow({
                 "unique_id": unique_id,
                 "address": addr,
@@ -138,10 +146,42 @@ with open("geocoded_output_34000.csv", "w", newline="", encoding="utf-8") as f:
                 "lon_gda94": lon_gda94
             })
 
-        time.sleep(1)   # Nominatim rate limit
+        time.sleep(0.25)   # Nominatim rate limit
 
-# Compute total time AFTER loop finishes
-total_time = time.time() - start_time
 
-print("Done. Results saved to geocoded_output_34000.csv")
-print(f"Total time taken: {total_time:.1f} seconds")
+
+if __name__ == "__main__":
+
+    start_time = time.time()
+
+    input_file = "all_addresses.csv"
+    count = 20_000
+
+    # first 20_000 i.e., 1 to 20_000 addresses
+    #start_line_number = 1
+
+
+    # second 20_000 i.e., 20_001 to 40_000 addresses
+    #start_line_number = 20_001
+
+
+
+    # this sample "addresses_34000.csv"
+    input_file = "addresses_34000.csv"
+    start_line_number = 1
+    count = 16_841
+
+
+    (output_file, start_line_number) = process_input_addresses(
+        input_file=input_file,
+        start_line_number=start_line_number,
+        count=count if count else 20_000,
+        start_time=start_time,
+        filename_suffix="34000"
+    )
+
+    # Compute total time AFTER loop finishes
+    total_time = time.time() - start_time
+
+    print(f"Done. Results saved to {output_file} (rows {1}-{start_line_number})")
+    print(f"Total time taken: {total_time:.1f} seconds")
