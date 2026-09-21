f17892b63380db9e68fa493fc16206969bb6aad2
catchspa <catchspa@gmail.com>
2026-09-21 23:06:48 +1000

    Add files via upload

---
+++ bin/gdal-dev-env.bat
@@ -0,0 +1,3 @@
+SET GDAL_DATA=%OSGEO4W_ROOT%\apps\gdal-dev\share\gdal
+SET GDAL_DRIVER_PATH=%OSGEO4W_ROOT%\apps\gdal-dev\lib\gdalplugins
+PATH %OSGEO4W_ROOT%\apps\gdal-dev\bin;%PATH%
---
+++ bin/gdal-dev-py-env.bat
@@ -0,0 +1,3 @@
+call gdal-dev-env.bat
+SET PYTHONPATH=%OSGEO4W_ROOT%\apps\gdal-dev\lib\site-packages;%PYTHONPATH%
+PATH %OSGEO4W_ROOT%\apps\gdal-dev\Scripts;%PATH%
