677136846f965dcd0b6a8430394ebc4de4de16f4
catchspa <catchspa@gmail.com>
2026-09-21 23:42:46 +1000

    Add files via upload

---
+++ lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-LC:/src/osgeo4w/src/libxslt/osgeo4w/install/lib"
+XSLT_LIBS="-lxslt -lxml2"
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-IC:/src/osgeo4w/src/libxslt/osgeo4w/install/include"
+MODULE_VERSION="xslt-1.1.39"
